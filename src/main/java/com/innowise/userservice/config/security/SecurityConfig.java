8bc81b5daeb7871d366065ea243ccb7859e22ae9
BadzeiPavel <pavelbadzei@gmail.com>
2026-05-29 20:12:26 +0300

    add test security config for integration tests

--- src/main/java/com/innowise/userservice/config/security/SecurityConfig.java
+++ src/main/java/com/innowise/userservice/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.innowise.commonstarter.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
+@Profile("!test")
 public class SecurityConfig {
 
   private final JwtTokenProvider jwtProvider;
